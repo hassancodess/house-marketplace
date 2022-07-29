8665bf81373c66a94572a1731a4ced5821981979
hassancodess <dev.hassancodes@gmail.com>
2022-07-29 23:24:53 +0500

    Add Sign In Functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,16 @@
+import { getAuth } from 'firebase/auth'
+import { useState, useEffect } from 'react'
 function Profile() {
-  return (
-    <div>
-      <h1>Profile</h1>
-    </div>
-  )
+  const [user, setUser] = useState(null)
+  const auth = getAuth()
+  useEffect(() => {
+    if (auth.currentUser) {
+      console.log(auth.currentUser)
+      setUser(auth.currentUser)
+    }
+  }, [])
+
+  return user ? <h1>{user.displayName}</h1> : <h1>No Profile Logged in</h1>
 }
 
 export default Profile
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 function SignIn() {
   const [showPassword, setShowPassword] = useState(false)
   const [formData, setFormData] = useState({
@@ -17,13 +18,29 @@ function SignIn() {
       [e.target.id]: e.target.value,
     }))
   }
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+      if (userCredential.user) {
+        navigate('/')
+      }
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
   return (
     <>
       <div className='pageContainer'>
         <header>
           <p className='pageHeader'>Welcome Back!</p>
         </header>
-        <form>
+        <form onSubmit={onSubmit}>
           <input
             type='text'
             placeholder='Email'
