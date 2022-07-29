1aa9d79ca6cc9deca180268831ed646e44692f64
hassancodess <dev.hassancodes@gmail.com>
2022-07-29 23:39:08 +0500

    Add React Toastify Container & CSS

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Explore from './pages/Explore'
 import Offers from './pages/Offers'
 import Profile from './pages/Profile'
@@ -8,18 +10,21 @@ import ForgotPassword from './pages/ForgotPassword'
 import Navbar from './components/Navbar'
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Explore />} />
-        <Route path='/offers' element={<Offers />} />
-        <Route path='/profile' element={<SignIn />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/forgot-password' element={<ForgotPassword />} />
-      </Routes>
-      {/* Navbar */}
-      <Navbar />
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+        </Routes>
+        {/* Navbar */}
+        <Navbar />
+      </Router>
+      <ToastContainer />
+    </>
   )
 }
 
