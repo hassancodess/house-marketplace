9385206dec0a2e8e21655388541d423ba5dc1186
hassancodess <dev.hassancodes@gmail.com>
2022-07-30 20:37:41 +0500

    Add Auth Status Hook

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -1,18 +1,24 @@
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 export const useAuthStatus = () => {
+  const isMounted = useRef(true)
   const [loggedIn, setLoggedIn] = useState(false)
   const [checkingStatus, setCheckingStatus] = useState(true)
   const auth = getAuth()
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setLoggedIn(true)
-      }
-      setCheckingStatus(false)
-    })
-  }, [])
+    if (isMounted) {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setLoggedIn(true)
+        }
+        setCheckingStatus(false)
+      })
+    }
+    return () => {
+      isMounted.current = false
+    }
+  }, [isMounted])
 
   return { loggedIn, checkingStatus }
 }
