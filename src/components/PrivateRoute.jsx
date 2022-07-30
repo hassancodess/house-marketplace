82c2e6ff7aea178dfc31aa2a9bf0b82c5fdefed6
hassancodess <dev.hassancodes@gmail.com>
2022-07-30 20:40:05 +0500

    Add Private Route Component

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,10 +1,11 @@
 import { Navigate, Outlet } from 'react-router-dom'
 import { useAuthStatus } from '../hooks/useAuthStatus'
+import Spinner from './Spinner'
 function PrivateRoute() {
   const { loggedIn, checkingStatus } = useAuthStatus()
 
   if (checkingStatus) {
-    return <h3>Loading...</h3>
+    return <Spinner />
   }
 
   return loggedIn ? <Outlet /> : <Navigate to='/sign-in' />
