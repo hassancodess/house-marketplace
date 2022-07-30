1b8e56b3db7b7b09e46e88c09fe5b33da173e054
hassancodess <dev.hassancodes@gmail.com>
2022-07-30 14:12:47 +0500

    Add Private Route Component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,12 @@
-import { getAuth } from 'firebase/auth'
+import { getAuth, updateEmail, updateProfile } from 'firebase/auth'
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { updateDoc, doc } from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { toast } from 'react-toastify'
 function Profile() {
   const auth = getAuth()
+  const [changeDetails, setChangeDetails] = useState(false)
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -15,16 +19,80 @@ function Profile() {
     auth.signOut()
     navigate('/')
   }
+  const onSumbit = () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        // Update in Auth
+        updateProfile(auth.currentUser, {
+          displayName: name,
+        })
+        // Update in Firestore
+        const userRef = doc(db, 'users', auth.currentUser.uid)
+        updateDoc(userRef, { name })
+      }
+      if (auth.currentUser.email !== email) {
+        // Update Email in Auth
+        updateEmail(auth.currentUser, email)
+        // Update Email in Firestore
+        const userRef = doc(db, 'users', auth.currentUser.uid)
+        updateDoc(userRef, { email })
+      }
+    } catch (error) {
+      toast.error(error)
+    }
+  }
+  const onChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value,
+    }))
+  }
 
   return (
     <>
       <div className='profile'>
-        <div className='profileHeader'>
+        <header className='profileHeader'>
           <p className='pageHeader'>My Profile</p>
           <button type='button' className='logOut' onClick={onLogOut}>
             Logout
           </button>
-        </div>
+        </header>
+        <main>
+          <div className='profileDetailsHeader'>
+            <p className='profileDetailsText'>Personal Details</p>
+            <p
+              className='changePersonalDetails'
+              onClick={() => {
+                changeDetails && onSumbit()
+                setChangeDetails((prev) => !prev)
+              }}
+            >
+              {changeDetails ? 'Done' : 'Change'}
+            </p>
+          </div>
+          <div className='profileCard'>
+            <form>
+              <input
+                type='text'
+                id='name'
+                className={!changeDetails ? 'profileName' : 'profileNameActive'}
+                disabled={!changeDetails}
+                value={name}
+                onChange={onChange}
+              />
+              <input
+                type='email'
+                id='email'
+                className={
+                  !changeDetails ? 'profileEmail' : 'profileEmailActive'
+                }
+                disabled={!changeDetails}
+                value={email}
+                onChange={onChange}
+              />
+            </form>
+          </div>
+        </main>
       </div>
     </>
   )
